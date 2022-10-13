c07b19a906c7bd4bbb2e14a5fadc11daedf5c760
Mariona <marianadal@escuelaces.net>
2022-10-13 09:43:17 +0200

    limpiando verborrea

--- MiGit.java
+++ MiGit.java
@@ -1,8 +1,6 @@
 public class MiGit {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
-		System.out.println("¿Qué tal?");
-		System.out.println("Yo muy bien");
 		System.out.println("Git mola mucho!");
 	}
 }
