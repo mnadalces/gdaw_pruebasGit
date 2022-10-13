81998cae4a90b2aa10327e70b4e498d7f4fdbe09
mnadalces <114143953+mnadalces@users.noreply.github.com>
2022-10-13 10:56:51 +0200

    Update MiGit.java

--- MiGit.java
+++ MiGit.java
@@ -2,6 +2,6 @@ public class MiGit {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Git mola mucho!");
-		System.out.println("Y el mundo ya lo sabe");
+		System.out.println("Y el mundo ya lo sabe muy bien");
 	}
 }
