093709b1c777d76b74fa90c488b7f2637f39722c
Mariona <marianadal@escuelaces.net>
2022-10-13 10:20:34 +0200

    hasta el final

--- MiGit.java
+++ MiGit.java
@@ -2,5 +2,6 @@ public class MiGit {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Git mola mucho!");
+		System.out.println("Y el mundo ya lo sabe");
 	}
 }
